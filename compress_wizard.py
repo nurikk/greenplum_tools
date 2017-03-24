bd1083093dbc303c7e4bdd263b85cd7788942f12
Ainur Timerbaev <nur.php@gmail.com>
2017-03-24 19:19:39 +0700

    remove junk

--- compress_wizard.py
+++ compress_wizard.py
@@ -15,14 +15,8 @@ def get_cursor(config):
     cursor =  conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     return cursor
 
-def format_seconds_to_readable_format(seconds):
-    return time.strftime("%H:%M:%S.{0}".format(round((seconds % 1)*1000)), time.gmtime(seconds))
-
 def out(cursor, sql, params = {}):
-    # print(cursor.mogrify(sql, params).decode('utf-8'), end=' ', flush=True)
-    # start = time.time()
     cursor.execute(sql, params)
-    # print('--', format_seconds_to_readable_format(time.time() - start), cursor.statusmessage)
     return_val = {}
     try:
         return_val = cursor.fetchall()
@@ -81,8 +75,7 @@ def bench_column(config, column):
                 pg_relation_size('compres_test_table'::regclass::oid) as size
             """.format(compresstype=compresstype, compresslevel=compresslevel, column_name=column['column_name'])
             size_info = out(curr, SIZE_SQL)[0]
-            results.append(size_info)
-            # print(column['column_name'], compresstype, compresslevel, size_info['size_h'])
+            results.append(size_info)            
             out(curr, 'drop table compres_test_table')
 
     sorted_results = sorted(results, key=lambda k: k['size'])
@@ -102,6 +95,7 @@ def make_magic(config):
     """
     table_info = out(curr, TABLE_DESC_SQL, config)
     for column in table_info:
+        #TODO: threads
         bench_column(config, column)
 
 
@@ -115,7 +109,7 @@ if __name__ == "__main__":
 
     parser.add_argument("-t", "--table", type=str, help="table name", required=True)
     parser.add_argument("-s", "--schema", type=str, help="schema name", required=True)
-    parser.add_argument("-l", "--lines", type=str, help="schema name", default=1000)
+    parser.add_argument("-l", "--lines", type=str, help="rows to examine", default=1000)
 
     params = parser.parse_args()
     make_magic(vars(params))
